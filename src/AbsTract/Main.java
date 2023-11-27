63e256e24a97bfc5f66d22903adac427752e2886
dyoas123 <rautrautjoash1@gmail.com>
2023-11-27 22:23:11 +0800

    ara diay

---
+++ src/AbsTract/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AbsTract;
+
+
+/**
+ *
+ * @author Edgar Caasalan
+ */
+public class Main {
+    
+    public static void main(String []args){
+        
+        Pets d = new Dog() ;
+        Pets c = new Cat();
+        
+    }
+}
